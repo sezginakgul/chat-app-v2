b8a7e7b362fcd550afaac5e852f35e1b305a33f2
sezgin <sezgin.akgull@gmail.com>
2023-04-15 01:48:30 +0300

    added backend files

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { default: axios } = require("axios");
+const { axios } = require("axios");
 
 require("dotenv").config();
 
@@ -10,15 +10,15 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-
+  // Get or create user on Chat Engine!
   try {
-    const response = await axios.put(
+    const r = await axios.put(
       "https://api.chatengine.io/users/",
-      { username, secret: username, first_name: username },
+      { username: username, secret: username, first_name: username },
       { headers: { "Private-Key": process.env.PRIVATE_KEY } }
     );
     return res.status(r.status).json(r.data);
-  } catch (error) {
+  } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
 });
